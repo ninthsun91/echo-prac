59f1cee70aed9cfe3e3288741df8137eb0a6167b
Ray Kim <ninthsun91@gmail.com>
2024-04-21 02:59:27 +0900

    🗃️ Connect database

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Seoul",
+		"localhost",
+		"root",
+		"qwe123123",
+		"test",
+		"5432",
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	DB = db
+
+	fmt.Println("Database connected!")
+}
--- server.go
+++ server.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"myapp/database"
 )
 
 func main() {
 	e := echo.New()
+	database.ConnectDatabase()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
