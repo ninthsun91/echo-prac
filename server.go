a9242509aec0ccca87dff7658ea91546ffe2ba67
Ray Kim <ninthsun91@gmail.com>
2024-04-21 14:26:25 +0900

    ✨ Users signup api - POST/users

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"myapp/database"
+	"myapp/models"
 )
 
 func main() {
@@ -23,5 +24,34 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	e.POST("/users", signupHandler)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type SignupRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func signupHandler(c echo.Context) error {
+	body := new(SignupRequest)
+	if err := c.Bind(&body); err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
+	}
+
+	user := models.User{
+		Name:     body.Name,
+		Email:    body.Email,
+		Password: body.Password,
+	}
+
+	result := database.DB.Create(&user)
+	if result.Error != nil {
+		c.Logger().Errorf("Failed to create user: %v", result.Error)
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	return c.JSON(http.StatusCreated, user)
 }
